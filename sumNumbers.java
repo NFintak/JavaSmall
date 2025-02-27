81e6ba8e63dc82e4b6513df6009c1cfaf0722d5a
Nathan <natefintak@gmail.com>
2025-02-27 16:44:15 -0500

    done with jshell and JavaSmall. fixed some typos

--- sumNumbers.java
+++ sumNumbers.java
@@ -1,10 +1,30 @@
-System.out.println("Hello, World!");
-System.out.println("Hello, World!");
-void println(String s) {
-    System.out.println(s);
+while (i <= 10) {
+    System.out.println(i);
+    i++;
 }
-println("your name here!");
-println("nathan");
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(5);
+sumNumbers(10)
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(5);
+sumNumbers(5)
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+while (i <= 10) {
+    System.out.println(i);
+    i++;
+}
 void printNumbers() {
     int i = 1;
     while (i <= 10) {
@@ -12,7 +32,6 @@ void printNumbers() {
         i++;
     }
 }
-printNumbers();
 void printNumbers(int n) {
     int i = 1;
     while (i <= n) {
@@ -23,6 +42,21 @@ void printNumbers(int n) {
 printNumbers(5);
 printNumbers(10);
 printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5);
+sumNumbers(5);
+sumNumbers(10)
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(5);
+sumNumbers(5)
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
 int sumNumbers(int n) {
     int sum = 0;
     for (int i = 1; i <= n; i++) {
@@ -37,11 +71,9 @@ sumNumbersTo10();
 int sumNumbersTo13() {
     return sumNumbers(13);
 }
-sumNumbersTo13();
-int addTwoNumber(int x, int y) {
+int addTwoNumbers(int x, int y) {
     return (x + y);
 }
-addTwoNumber(5, 10);
 int divideTwoNumbers(int x, int y) {
     return (x / y);
 }
@@ -51,31 +83,24 @@ int multiplyTwoNumbers(int x, int y) {
 int subtractTwoNumbers(int x, int y) {
     return (x - y);
 }
-divideTwoNumbers(45, 15);
-multiplyTwoNumbers(3, 5);
-subtractTwoNumbers(20,5);
+isEven(5)
+isOdd(5);
 var x = 5;
 var y = 2;
-x % y;
-boolean isOdd(int x) {
-    if (x % 2 == 1) {
-        return true;
-    } else {
-        return false;
-    }
-}
+x % y
 int i = 1;
 while (i <= 10) {
     System.out.println(i + " % 2 = " + i % 2);
     i++;
 }
 boolean isEven(int x) {
-    if (x % 2 == 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return x % 2 == 0;
 }
+boolean isOdd(int x) {
+    return x % 2 == 1;
+}
+printOddEven();
+printOddEven();
 void printOddEven() {
     int i = 1;
     while (i <= 10) {
@@ -87,5 +112,4 @@ void printOddEven() {
         i++;
     }
 }
-sumNumbers(5);
-sumNumbersTo10();
+printOddEven();
